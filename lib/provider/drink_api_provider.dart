343e5bea21f50afa167b7e25c099e4db6479ea08
BogdanVashkevich13 <bogdanvashkevich@mail.tu>
2022-10-25 18:09:37 +0300

    0

--- lib/provider/drink_api_provider.dart
+++ lib/provider/drink_api_provider.dart
@@ -30,13 +30,15 @@ class DrinkApi extends StateNotifier<Drink> {
     }
   }
 
-  Future<Drink> getDrinkOptions(String id) async {
+  Future<List<DrinkOption>> getDrinkOptions(String id) async {
     var url = Uri.https('https://www.thecocktaildb.com/api/json/v1/1/');
     var response = await get(url);
     if (response.statusCode == 200) {
-      final items = json.decode(response.body);
-      Drink drinkDetails = Drink.fromJson(items);
-      return drinkDetails;
+      final items = json.decode(response.body)['drinks'];
+      List<DrinkOption> listOfUsers = items.map<DrinkOption>((json) {
+        return DrinkOption.fromJson(json);
+      }).toList();
+      return listOfUsers;
     } else {
       throw Exception('Failed to load internet');
     }
