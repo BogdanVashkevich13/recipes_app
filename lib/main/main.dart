1d10a2c735ecbcc88bd0b530727ee60de98031d6
BogdanVashkevich13 <bogdanvashkevich@mail.tu>
2022-11-01 19:13:31 +0300

    0

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,15 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recipes_bloc/Pages/home_page.dart';
+import 'package:recipes_bloc/Pages/loading_screen.dart';
 
-import '../Pages/alcoholic_page.dart';
-import '../Pages/cocktails_page.dart';
-import '../Pages/non_alcoholic_page.dart';
-import '../Pages/ordinary_drink_page.dart';
-import '../Wigets/list_drinks.dart';
-import '../firstScreen/first_screen.dart';
-import '../startScreen/start_screen.dart';
+
+
 void main()  {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -25,18 +22,12 @@ class MyApp extends StatelessWidget {
         ),
         initialRoute: '/',
         routes: {
-          '/':(BuildContext context) => const startScreen(),
-          '/firstScreen':(BuildContext context) => const firstScreen(),
-          '/alcoholicPage':(BuildContext context) => const alcoholicPage(),
-          '/nonAlcoholicPage':(BuildContext context) => const nonAlcoPage(),
-          '/ordinaryPage':(BuildContext context) => const ordinaryPage(),
-          '/cocktailsPage':(BuildContext context) => const cocktailsPage (),
-          //'/randomPage':(BuildContext context) => const alcoholicPage(),
+          '/':(BuildContext context) => const loadingScreen(),
+          '/HomePage': (BuildContext context) => const HomePage(),
         }
     );
   }
 }
-
 
 
 
