024e4501bdc440275ad020f22c6c99c15d176f83
BogdanVashkevich13 <bogdanvashkevich@mail.tu>
2022-11-02 15:42:07 +0300

    0

--- lib/Wigets/list_drinks.dart
+++ lib/Wigets/list_drinks.dart
@@ -7,95 +7,90 @@ import 'drink_details.dart';
 import '../Api/drink_option.dart';
 
 class ListDrinks extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  final String search;
+  final String title;
+
+  const ListDrinks({ Key? key,  required this.search,  required this.title}): super(key: key);
+  _ListDrinksState createState() => _ListDrinksState();
 }
 
+class _ListDrinksState extends State<ListDrinks> {
 
+  Future<List<DrinkOption>> _getDrinkOptions() async {
+    return Repository().getDrinkOptions(widget.search);
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   final String search;
-//   final String title;
-//
-//   const ListDrinks({ Key? key,  required this.search,  required this.title}): super(key: key);
-//   _ListDrinksState createState() => _ListDrinksState();
-// }
-//
-// class _ListDrinksState extends State<ListDrinks> {
-//
-//   Future<List<DrinkOption>> _getDrinkOptions() async {
-//     return Repository().getDrinkOptions(widget.search);
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title,
-//           style: const TextStyle(
-//             color: ColorsSet.black,
-//             fontStyle: FontStyle.italic,
-//             fontSize: 25,
-//             fontWeight: FontWeight.bold,
-//           ),
-//         ),
-//         backgroundColor: ColorsSet.red,
-//         centerTitle: true,
-//       ),
-//       backgroundColor: Colors.black,
-//       body: FutureBuilder<List<DrinkOption>>(
-//         future: _getDrinkOptions(),
-//         builder: (context, snapshot) {
-//           if (!snapshot.hasData) {
-//             return const Center(child: CircularProgressIndicator(
-//               color: ColorsSet.white,
-//             ));
-//           }
-//           return ListView(
-//             children: snapshot.data!.map((drink) => ListTile(
-//               // leading: const CircleAvatar(
-//               // ),
-//               title: Text(drink.strDrink),
-//               onTap: () {
-//                 Navigator.push(
-//                   context,
-//                   PageTransition(child: DrinkDetails(id: drink.idDrink, drinkName: drink.strDrink,),
-//                       type: PageTransitionType.rightToLeft
-//                   ),
-//                 );
-//               },
-//             )) .toList(),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title,
+          style: const TextStyle(
+            color: ColorsSet.black,
+            fontStyle: FontStyle.italic,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: ColorsSet.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: FutureBuilder<List<DrinkOption>>(
+        future: _getDrinkOptions(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator(
+              color: ColorsSet.white,
+            ));
+          }
+          return ListView(
+            children: snapshot.data!.map((drink) => ListTile(
+              // leading: const CircleAvatar(
+              // ),
+              title: Text(drink.strDrink),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  PageTransition(child: DrinkDetails(id: drink.idDrink, drinkName: drink.strDrink,),
+                      type: PageTransitionType.rightToLeft
+                  ),
+                );
+              },
+            )) .toList(),
+          );
+        },
+      ),
+    );
+  }
+}
